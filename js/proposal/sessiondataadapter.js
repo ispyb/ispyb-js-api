f1e9a329ede97eff763f7b5b7bccf799029a6450
Alejandro De Maria Antolinos <demariaa@lindemaria.esrf.fr>
2016-03-02 13:33:12 +0100

    Remove private attribute on bower.json

--- js/proposal/sessiondataadapter.js
+++ js/proposal/sessiondataadapter.js
@@ -12,6 +12,7 @@ SessionDataAdapter.prototype.getSessions = function(){
 
 
 
+
 SessionDataAdapter.prototype.getSessionsByDate = function(startDate, endDate){
 	this.get('/{token}/proposal/session/list?startdate={0}&enddate={1}'.format([startDate, endDate]));
 };
