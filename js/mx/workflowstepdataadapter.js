f713a3b4af824bccf599f15d4f4a0c984438d7e0
Alejandro De Maria Antolinos <demariaa@lindemaria.esrf.fr>
2016-03-09 18:06:57 +0100

    Fixed: - getUrl for html, images and result instead of get - Datacollection getAcronym was accessing to list instead of view

--- js/mx/workflowstepdataadapter.js
+++ js/mx/workflowstepdataadapter.js
@@ -7,15 +7,15 @@ WorkflowStepDataAdapter.prototype.post = DataAdapter.prototype.post;
 WorkflowStepDataAdapter.prototype.getUrl = DataAdapter.prototype.getUrl;
 
 WorkflowStepDataAdapter.prototype.getImageByWorkflowStepId = function(workflowStepId){
-	 this.get('/{token}/proposal/{proposal}/mx/workflow/step/{0}/image'.format( [workflowStepId.toString()]));
+	 this.getUrl('/{token}/proposal/{proposal}/mx/workflow/step/{0}/image'.format( [workflowStepId.toString()]));
 };
 
 WorkflowStepDataAdapter.prototype.getHtmlByWorkflowStepId = function(workflowStepId){
-	 this.get('/{token}/proposal/{proposal}/mx/workflow/step/{0}/html'.format( [workflowStepId.toString()]));
+	 this.getUrl('/{token}/proposal/{proposal}/mx/workflow/step/{0}/html'.format( [workflowStepId.toString()]));
 };
 
 WorkflowStepDataAdapter.prototype.getResultByWorkflowStepId = function(workflowStepId){
-	 this.get('/{token}/proposal/{proposal}/mx/workflow/step/{0}/result'.format( [workflowStepId.toString()]));
+	 this.getUrl('/{token}/proposal/{proposal}/mx/workflow/step/{0}/result'.format( [workflowStepId.toString()]));
 };
 
 WorkflowStepDataAdapter.prototype.getWorkflowstepByIdList = function(workflowStepIds){
