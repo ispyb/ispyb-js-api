666b905aed58f1fa1ad596e24828550e50f0be18
Alejandro De Maria Antolinos <demariaa@lindemaria.esrf.fr>
2016-03-18 11:57:33 +0100

    This closes #6

--- test/config.js
+++ test/config.js
@@ -6,10 +6,28 @@ var Config = {
 					url 		: "http://ispyvalid.esrf.fr:8080/ispyb/ispyb-ws/rest",
 					proposal	: "mx415"
 				}
-			],
+	],
+	data :{
+			labcontacts : {
+						labContactId 		: 149,
+						cardName		: {
+										original : "KOSKI-Department of B",
+										test	 : "[QUNIT TEST] KOSKI-Department of B"
+						},
+						courierAccount		: {
+										original : "246824029",
+										test	 : "[QUNIT TEST] 246824029"
+
+						}
+			
+			}
+
+
+	},
 	test : function(){
 		new MacromoleculeTest().init();
 		new BufferTest().init();
+		new LabcontactTest().init();
 	}
 
 };
