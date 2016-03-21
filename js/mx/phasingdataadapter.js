e8c72d6eec85c5877041c82f856aaf89cae1fb4d
Alejandro De Maria Antolinos <demariaa@lindemaria.esrf.fr>
2016-03-21 17:03:00 +0100

    It closes #10

---
+++ js/mx/phasingdataadapter.js
@@ -0,0 +1,22 @@
+/**
+* Interface implementing the API for phasing
+*
+* @class MyClass
+* @constructor
+*/
+function PhasingDataAdapter(args){
+	DataAdapter.call(this, args);
+}
+
+PhasingDataAdapter.prototype.get = DataAdapter.prototype.get;
+PhasingDataAdapter.prototype.post = DataAdapter.prototype.post;
+PhasingDataAdapter.prototype.getUrl = DataAdapter.prototype.getUrl;
+
+/**
+* It retrieves the phasing view from ISPyB
+* @method getViewByDataCollectionId
+* @param {String} dataCollectionId It may be a comma-separated list of data collection ids
+*/
+PhasingDataAdapter.prototype.getViewByDataCollectionId= function(dataCollectionId){
+	this.get('/{token}/proposal/{proposal}/mx/phasing/datacollection/{0}/view'.format( [dataCollectionId]));
+};
