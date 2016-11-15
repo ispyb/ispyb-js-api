d425e61e6e92985c019037feac2ff51aa8781231
Alejandro De Maria Antolinos <demariaa@lindemaria.esrf.fr>
2016-11-15 14:26:32 +0100

    Added phasing by dataCollectionGroupId

--- js/mx/phasingdataadapter.js
+++ js/mx/phasingdataadapter.js
@@ -21,7 +21,7 @@ PhasingDataAdapter.prototype.getUrl = DataAdapter.prototype.getUrl;
 * @param {String} autoprocIntegrationId It may be a comma-separated list of autoprocIntegrationId
 */
 PhasingDataAdapter.prototype.getPhasingViewByAutoProcIntegrationId = function(autoprocIntegrationId){
-	this.get('/{token}/proposal/{proposal}/mx/phasing/autoprocIntegrationId/{0}/list'.format( [autoprocIntegrationId]));
+	this.get('/{token}/proposal/{proposal}/mx/phasing/autoprocintegrationid/{0}/list'.format( [autoprocIntegrationId]));
 };
 
 /**
@@ -30,8 +30,18 @@ PhasingDataAdapter.prototype.getPhasingViewByAutoProcIntegrationId = function(au
 * @param {String} dataCollectionId It may be a comma-separated list of data collections
 */
 PhasingDataAdapter.prototype.getPhasingViewByDataCollectionId = function(dataCollectionId){
-	this.get('/{token}/proposal/{proposal}/mx/phasing/dataCollectionId/{0}/list'.format( [dataCollectionId]));
+	this.get('/{token}/proposal/{proposal}/mx/phasing/datacollectionid/{0}/list'.format( [dataCollectionId]));
 };
+
+/**
+* It retrieves the phasing view
+* @method getPhasingViewByDataCollectionId
+* @param {String} dataCollectionGroupId It may be a comma-separated list of data dataCollection Group
+*/
+PhasingDataAdapter.prototype.getPhasingViewByDataCollectionGroupId = function(dataCollectionGroupId){
+	this.get('/{token}/proposal/{proposal}/mx/phasing/datacollectiongroupid/{0}/list'.format( [dataCollectionGroupId]));
+};
+
 
 /**
 * It retrieves the phasing view
@@ -39,7 +49,7 @@ PhasingDataAdapter.prototype.getPhasingViewByDataCollectionId = function(dataCol
 * @param {String} sampleId It may be a comma-separated list of sample ids
 */
 PhasingDataAdapter.prototype.getPhasingViewBySampleId = function(sampleId){
-	this.get('/{token}/proposal/{proposal}/mx/phasing/sampleId/{0}/list'.format( [sampleId]));
+	this.get('/{token}/proposal/{proposal}/mx/phasing/sampleid/{0}/list'.format( [sampleId]));
 };
 
 /**
@@ -48,7 +58,7 @@ PhasingDataAdapter.prototype.getPhasingViewBySampleId = function(sampleId){
 * @param {String} proteinId It may be a comma-separated list of protein ids
 */
 PhasingDataAdapter.prototype.getPhasingViewByProteinId = function(proteinId){
-	this.get('/{token}/proposal/{proposal}/mx/phasing/proteinId/{0}/list'.format( [proteinId]));
+	this.get('/{token}/proposal/{proposal}/mx/phasing/proteinid/{0}/list'.format( [proteinId]));
 };
 
 /**
@@ -57,7 +67,7 @@ PhasingDataAdapter.prototype.getPhasingViewByProteinId = function(proteinId){
 * @param {String} sessionId It may be a comma-separated list of session ids
 */
 PhasingDataAdapter.prototype.getPhasingViewBySessionId = function(sessionId){
-	this.get('/{token}/proposal/{proposal}/mx/phasing/sessionId/{0}/list'.format( [sessionId]));
+	this.get('/{token}/proposal/{proposal}/mx/phasing/sessionid/{0}/list'.format( [sessionId]));
 };
 
 /**
@@ -66,9 +76,17 @@ PhasingDataAdapter.prototype.getPhasingViewBySessionId = function(sessionId){
 * @param {String} phasingStepId It may be a comma-separated list of session ids
 */
 PhasingDataAdapter.prototype.getPhasingViewByPhasingStepId = function(phasingStepId){
-	this.get('/{token}/proposal/{proposal}/mx/phasing/phasingStepId/{0}/list'.format( [phasingStepId]));
+	this.get('/{token}/proposal/{proposal}/mx/phasing/phasingstepid/{0}/list'.format( [phasingStepId]));
 };
 
+/**
+* It retrieves the phasing view
+* @method getPhasingViewByPhasingStepId
+* @param {String} phasingStepId It may be a comma-separated list of session ids
+*/
+PhasingDataAdapter.prototype.getDownloadFilesByPhasingStepIdURL = function(phasingStepId){
+	return this.getUrl('/{token}/proposal/{proposal}/mx/phasing/phasingstepid/{0}/download'.format( [phasingStepId]));
+};
 
 /**
 * It retrieves the phasing files by phasing step
@@ -76,7 +94,7 @@ PhasingDataAdapter.prototype.getPhasingViewByPhasingStepId = function(phasingSte
 * @param {String} phasingStepId It may be a comma-separated list of session ids
 */
 PhasingDataAdapter.prototype.getPhasingFilesByPhasingStepId = function(phasingStepId){
-	this.get('/{token}/proposal/{proposal}/mx/phasing/phasingStepId/{0}/files'.format( [phasingStepId]));
+	this.get('/{token}/proposal/{proposal}/mx/phasing/phasingstepid/{0}/files'.format( [phasingStepId]));
 };
 
 /**
@@ -85,5 +103,14 @@ PhasingDataAdapter.prototype.getPhasingFilesByPhasingStepId = function(phasingSt
 * @param {String} phasingStepId It may be a comma-separated list of session ids
 */
 PhasingDataAdapter.prototype.downloadPhasingFilesByPhasingAttachmentId = function(phasingAttachmentId){
-	return this.getUrl('/{token}/proposal/{proposal}/mx/phasing/phasingProgramAttachmentId/{0}/download'.format( [phasingAttachmentId]));
+	return this.getUrl('/{token}/proposal/{proposal}/mx/phasing/phasingprogramattachmentid/{0}/download'.format( [phasingAttachmentId]));
+};
+
+/**
+* It downloads a csv file by phasing step attachment id
+* @method getPhasingFilesByPhasingStepId
+* @param {String} phasingStepId It may be a comma-separated list of session ids
+*/
+PhasingDataAdapter.prototype.getCSVPhasingFilesByPhasingAttachmentIdURL = function(phasingAttachmentId){
+	return this.getUrl('/{token}/proposal/{proposal}/mx/phasing/phasingprogramattachmentid/{0}/csv'.format( [phasingAttachmentId]));
 };
