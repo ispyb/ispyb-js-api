69fa5419a06ca9a5c1ebaf484b013a51c5094d36
Alejandro De Maria Antolinos <demariaa@lindemaria.esrf.fr>
2016-12-21 16:21:02 +0100

    New method saving crystal forms

--- js/mx/crystaldataadapter.js
+++ js/mx/crystaldataadapter.js
@@ -30,12 +30,43 @@ CrystalDataAdapter.prototype.getCrystalById= function(crystalId){
 * then it is possible to retrieve all cell dimensions from the geometry class
 */
 CrystalDataAdapter.prototype.getGeometryclassBySpacegroup= function(spacegroup){
-	 this.get('/{token}/mx/geometryclass/{0}/get'.format( [spacegroup]));
+	 this.get('/{token}/proposal/{proposal}/mx/crystal/geometryclass/{0}/list'.format( [spacegroup]));
 };
 
 
 
-
+/**
+* @method save
+* Create and new crystal form or updates an existing one
+*/
+CrystalDataAdapter.prototype.save= function(
+                                                proteinId,
+                                                crystalId,
+                                                name,
+                                                spaceGroup,
+                                                cellA,
+                                                cellB,
+                                                cellC,
+                                                cellAlpha,
+                                                cellBeta,
+                                                cellGamma,
+                                                comments)
+{
+    var url = ('/{token}/proposal/{proposal}/crystal/save'.format([experimentId]));
+	this.post(url, {
+						proteinId:proteinId,
+                        crystalId:crystalId,
+                        name:name,
+                        spaceGroup:spaceGroup,
+                        cellA:cellA,
+                        cellB:cellB,
+                        cellC:cellC,
+                        cellAlpha:cellAlpha,
+                        cellBeta:cellBeta,
+                        cellGamma:cellGamma,
+                        comments:comments
+	});
+};
 
 
 
