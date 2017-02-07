4ed169857130f40da9578f71720819a24a925dc7
GONZALEZ Sergio <SGONZALE@esrf.fr>
2017-02-07 14:13:55 +0100

    Added method getStatisticsByDateAndBeamline

--- js/mx/statisticsdataadapter.js
+++ js/mx/statisticsdataadapter.js
@@ -16,9 +16,22 @@ StatisticsDataAdapter.prototype.getUrl = DataAdapter.prototype.getUrl;
 /**
 * It retrieves the statistics url between the given dates
 * @method getPhasingFilesByPhasingProgramAttachmentIdAsImage
+* @param {String} type
 * @param {String} startDate
 * @param {String} endDate
 */
-StatisticsDataAdapter.prototype.getStatisticsByDate = function(startDate,endDate){
-	return this.getUrl('/{token}/stats/autoprocstatistics/innerShell/{0}/{1}/csv'.format( [startDate,endDate]));                                                    
+StatisticsDataAdapter.prototype.getStatisticsByDate = function(type,startDate,endDate){
+	return this.getUrl('/{token}/stats/autoprocstatistics/{0}/{1}/{2}/csv'.format( [type,startDate,endDate]));                                                    
+};
+
+/**
+* It retrieves the statistics url between the given dates for the given beamline
+* @method getPhasingFilesByPhasingProgramAttachmentIdAsImage
+* @param {String} type
+* @param {String} startDate
+* @param {String} endDate
+* @param {String} beamline
+*/
+StatisticsDataAdapter.prototype.getStatisticsByDateAndBeamline = function(type,startDate,endDate,beamline){
+	return this.getUrl('/{token}/stats/autoprocstatistics/{0}/{1}/{2}/csv?beamline={3}'.format( [type,startDate,endDate,beamline]));                                                    
 };
